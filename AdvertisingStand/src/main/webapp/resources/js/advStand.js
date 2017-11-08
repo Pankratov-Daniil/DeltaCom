6b1fae0efb8f8db131a883495c7d6e1dac57b9e4
Daniil <pankratov.daniil.v@gmail.com>
2017-11-09 00:14:44 +0300

    Rewrited view of advertising stand. Looks pretty good.

--- AdvertisingStand/src/main/webapp/resources/js/advStand.js
+++ AdvertisingStand/src/main/webapp/resources/js/advStand.js
@@ -1,16 +1,26 @@
-var app = angular.module('advStand', ['ui.bootstrap']);
+var app = angular.module('advStand', ['ngAnimate']);
 
-app.controller('TariffsController', function($scope, $http) {
-    $scope.carouselInterval = 2000;
-    $scope.carouselActiveSlide = 0;
+app.controller('TariffsController', function($scope, $http, $interval, $timeout) {
+    $scope.tariffSwapInterval = 3000;
     $scope.tariffs = [];
-    var dataToFunc = {http : $http, scope : $scope};
+    $scope.index = 0;
+    $scope.isStart = true;
+    $scope.iter = 0;
+    var dataToFunc = {http : $http, scope : $scope, timeout: $timeout};
     var uri = "ws://" + document.location.host + document.location.pathname + "tariffSubscription";
     var webSocket = new WebSocket(uri);
 
     webSocket.onopen = function (evt) { console.log("Connected to tariffSubscription socket!") };
     webSocket.onmessage = function (evt) { updateTariffs(dataToFunc); };
     updateTariffs(dataToFunc);
+
+    $interval(function() {
+        $scope.iter = ($scope.iter + 1) % $scope.tariffs.length;
+        $scope.index = -1;
+        $timeout(function () {
+            $scope.index = $scope.tariffs[$scope.iter].id;
+        }, 500);
+    }, $scope.tariffSwapInterval);
 });
 
 function updateTariffs(data) {
@@ -20,6 +30,12 @@ function updateTariffs(data) {
                     item.price = item.price.toLocaleString('ru-RU', {style: 'currency', currency : 'RUB'});
                 });
                 data.scope.tariffs = response.data;
+                data.scope.index = response.data.length > 0 ? response.data[0].id : -1;
+                data.scope.iter = 0;
+                data.scope.isStart = true;
+                data.timeout(function () {
+                    data.scope.isStart = false;
+                }, 601);
                 console.log("Tariffs updated!");
             },
             function (response) {
